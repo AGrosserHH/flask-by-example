02ae2308fbc9d77c08c4bd20d718e11ae711f61a
Andreas Grosser <andreas.grosser@kantar.com>
2020-11-28 14:21:43 +0100

    updates

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 # https://realpython.com/flask-by-example-part-1-project-setup/
 
-import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-print(os.environ['APP_SETTINGS'])
+from models import Result
+
 
 @app.route('/')
 def hello():
@@ -17,6 +21,7 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
